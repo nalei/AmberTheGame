8cb0182f1158b85dc0b9e94e943af73e88772034
Yuri Anikin <anikin.yu@gmail.com>
2020-01-14 23:40:21 +0300

    wip

--- AmberTheGame/Player Control/TouchControlInputNode.swift
+++ AmberTheGame/Player Control/TouchControlInputNode.swift
@@ -1,13 +1,15 @@
 import SpriteKit
 
 
-// Протокол управления событиями
 protocol ControlInputSourceDelegate : class {
   func follow(command : String?)
 }
 
+/// Виртуальный геймпад
 class TouchControlInputNode: SKSpriteNode {
-  var inputDelegate : ControlInputSourceDelegate?
+  // MARK: Properties
+  
+  weak var inputDelegate : ControlInputSourceDelegate?
   
   var alphaUnpressed:CGFloat = 0.8
   var alphaPressed:CGFloat   = 1
@@ -18,6 +20,8 @@ class TouchControlInputNode: SKSpriteNode {
   let buttonDirLeft  = SKSpriteNode(imageNamed: "button-left")
   let buttonDirRight = SKSpriteNode(imageNamed: "button-right")
   let buttonA        = SKSpriteNode(imageNamed: "button-A")
+  
+  // MARK: Initialization
   
   init(frame: CGRect) {
     super.init(texture: nil, color: UIColor.clear, size: frame.size)
@@ -55,75 +59,88 @@ class TouchControlInputNode: SKSpriteNode {
     self.addChild(button)
   }
   
-  func setAlphaStatus(_ button: SKSpriteNode) {
-    if pressedButtons.firstIndex(of: button) == nil {
-      button.alpha = alphaUnpressed
-    } else {
+  // MARK: UIResponder
+  
+  func buttonPressed(_ button: SKSpriteNode) {
+    let insertionResult = pressedButtons.insert(button)
+    if insertionResult.inserted {
       button.alpha = alphaPressed
+      
+      if let buttonName = button.name {
+        inputDelegate?.follow(command: "\(String(describing: buttonName))")
+      }
+    }
+  }
+  
+  func buttonUnpressed(_ button: SKSpriteNode) {
+    if let _ = pressedButtons.remove(button) {
+      button.alpha = alphaUnpressed
+      
+      if let buttonName = button.name {
+        inputDelegate?.follow(command: "stop \(String(describing: buttonName))")
+      }
     }
   }
   
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesBegan(touches, with: event)
+    
     for touch in touches {
       let touchPoint = touch.location(in: self)
       
       for button in allButtons {
-        
-        if button.contains(touchPoint) && !pressedButtons.contains(button) {
-          pressedButtons.insert(button)
-          inputDelegate?.follow(command: button.name!)
+        if button.contains(touchPoint) {
+          buttonPressed(button)
         }
-        
-        setAlphaStatus(button)
       }
     }
   }
   
   override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesMoved(touches, with: event)
+    
     for touch in touches {
       let touchPoint = touch.location(in: self)
       let previousTouchPoint = touch.previousLocation(in: self)
       
       for button in allButtons {
         
-        // если касание покидает кнопку
-        if button.contains(previousTouchPoint) && !button.contains(touchPoint) {
-          if let _ = pressedButtons.remove(button) {
-            inputDelegate?.follow(command: "cancel \(String(describing: button.name!))")
-          }
+        // Если касание перемещается на кнопку которая ещё не нажата
+        if !button.contains(previousTouchPoint) && button.contains(touchPoint) {
+          buttonPressed(button)
         }
-        // если касание перемещается на кнопку которая ещё не нажата
-        else if !button.contains(previousTouchPoint) && button.contains(touchPoint) && !pressedButtons.contains(button) {
-          pressedButtons.insert(button)
-          inputDelegate?.follow(command: button.name!)
+        // Если касание покидает кнопку
+        else if button.contains(previousTouchPoint) && !button.contains(touchPoint) {
+          buttonUnpressed(button)
         }
-        
-        setAlphaStatus(button)
       }
     }
   }
   
   override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesEnded(touches, with: event)
+    
     touchStop(touches: touches)
   }
   
   override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesCancelled(touches, with: event)
+    
     touchStop(touches: touches)
   }
   
   func touchStop(touches: Set<UITouch>) {
+    // Если нет нажатых кнопок, выходим немедленно
+    guard !pressedButtons.isEmpty else { return }
+
     for touch in touches {
       let touchPoint = touch.location(in: self)
-      
+
       for button in allButtons {
-        
+
         if button.contains(touchPoint) {
-          if let _ = pressedButtons.remove(button) {
-            inputDelegate?.follow(command: "stop \(String(describing: button.name!))")
-          }
+          buttonUnpressed(button)
         }
-        
-        setAlphaStatus(button)
       }
     }
   }
